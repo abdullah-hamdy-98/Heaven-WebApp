ea92c4b134a6280d1f4975d3f4d9a6d59069ed29
abdullah-hamdy-98 <abdo1231992260@gmail.com>
2024-11-17 02:14:20 +0200

    SiteSloa

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -3,10 +3,10 @@ import Image from "next/image"
 function Topbar() {
     return (
         <div className="py-12 px-24 bg-light-3">
-            <Image src='/Logo/logo.png'
-                alt="Logo"
+            <Image src='/logo/logo.png'
+                alt="Site Logo"
                 width={200}
-                height={200}
+                height={50}
             />
         </div>
     )
