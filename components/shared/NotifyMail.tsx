e7e891d7c7634fb9b46d301fe33c354e5a0ece46
abdullah-hamdy-98 <abdo1231992260@gmail.com>
2024-11-22 15:44:33 +0200

    ghhh

--- components/shared/NotifyMail.tsx
+++ components/shared/NotifyMail.tsx
@@ -70,7 +70,7 @@ function Notify() {
 
             <div className="flex items-center justify-center mt-6 space-x-4">
                 <a
-                    href="https://facebook.com"
+                    href="https://www.facebook.com/heavenagency0?mibextid=LQQJ4d"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-1 hover:text-blue transition duration-200"
@@ -79,7 +79,7 @@ function Notify() {
                     <i className="fab fa-facebook fa-x"></i>
                 </a>
                 <a
-                    href="https://instagram.com"
+                    href="https://www.instagram.com/heavenagency.eg?igsh=ajlqbTBoM2Q0dGdt"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-1 hover:text-pink-500 transition duration-200"
@@ -88,13 +88,13 @@ function Notify() {
                     <i className="fab fa-instagram fa-x"></i>
                 </a>
                 <a
-                    href="https://twitter.com"
+                    href="https://www.behance.net/heavenagency2"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-1 hover:text-black transition duration-200"
                     aria-label="Twitter"
                 >
-                    <i className="fab fa-x-twitter fa-x"></i>
+                    <i className="fa-brands fa-behance"></i>
                 </a>
             </div>
 
